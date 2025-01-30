eda7763d684da067303bb47bdb62fe5d9808735e
Aaron Wolf <aaron.wolf@gmail.com>
2025-01-30 16:17:13 +0200

    schedule calendar

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -4,10 +4,24 @@ import Par from '../components/Par';
 
 export default function Schedule() {
   const lang = useLanguageStore((state) => state.lang);
+
   return (
     <>
       <Par type='h1' text={consts.schedule[lang].title} />
-      <Par type='p' text={consts.schedule[lang].content} />
+      {/* <Par type='p' text={consts.schedule[lang].content} /> */}
+      <iframe
+        loading='lazy'
+        style={{
+          width: '100%',
+          height: '700px',
+          top: '0',
+          left: '0',
+          border: 'none',
+          padding: '0',
+          margin: '0'
+        }}
+        src='https://www.canva.com/design/DAGaxVIehac/atI8pr4vJoKb-xfsSPkWYA/view?embed'
+      ></iframe>
     </>
   );
 }
